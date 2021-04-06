93e598ab45f19ac9bb83e59ec098bfe9906b36d2
georgeabdulaziz <george.abdulaziza@ryerson.ca>
2021-04-05 22:25:42 -0400

    running index.html from flask demo api

--- flask_demo_api.py
+++ flask_demo_api.py
@@ -1,5 +1,6 @@
 #!flask/bin/python
 from flask import Flask, jsonify
+from flask import Flask, render_template
 app = Flask(__name__)
 tasks = [
     {
@@ -75,7 +76,8 @@ def get_tasks():
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    #return "Hello, World!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
